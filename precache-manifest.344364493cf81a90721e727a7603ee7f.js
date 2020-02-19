fecfec5a6c7a37b4db19c591e05b139016f5d4c5
Jeus Santiago <jeus1santiago@gmail.com>
2020-02-18 19:19:37 -0500

    Updates

---
+++ precache-manifest.344364493cf81a90721e727a7603ee7f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a3a7b67a84f98e4a125e9ec1cce3782",
+    "url": "/retail-landing-page/index.html"
+  },
+  {
+    "revision": "775918735316a0e2d580",
+    "url": "/retail-landing-page/static/css/2.aca86c19.chunk.css"
+  },
+  {
+    "revision": "846318470506596b4afe",
+    "url": "/retail-landing-page/static/css/main.0578a28f.chunk.css"
+  },
+  {
+    "revision": "775918735316a0e2d580",
+    "url": "/retail-landing-page/static/js/2.23099377.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/retail-landing-page/static/js/2.23099377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "846318470506596b4afe",
+    "url": "/retail-landing-page/static/js/main.2816c81a.chunk.js"
+  },
+  {
+    "revision": "c8008f83f8c77628db64",
+    "url": "/retail-landing-page/static/js/runtime-main.5ea8bb9c.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/retail-landing-page/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/retail-landing-page/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/retail-landing-page/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/retail-landing-page/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/retail-landing-page/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/retail-landing-page/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/retail-landing-page/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
